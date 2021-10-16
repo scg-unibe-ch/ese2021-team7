7c02ea0dc3c95276eea4a40e1eac205bcc47b7f2
Danilo Bigovic <d.big.be@gmail.com>
2021-10-16 20:07:41 +0200

    Add methods for: - fetching a post by it's id - fetching all posts - creating a post
    ATTENTION: Tests not yet working

--- ese2021-project-scaffolding/scaffolding/backend/src/services/post.service.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/services/post.service.ts
@@ -1,29 +1,33 @@
 import { Post, PostAttributes } from '../models/post.model';
 import {User} from '../models/user.model';
+import { where } from 'sequelize';
 
 
 export class PostService {
-    public getPostById(postId: number): Promise<Post> {
-        // TODO: Implement logic
-        return Promise.reject();
+    // Should this method be static?
+    public async getPostById(postId: number): Promise<Post> {
+        const postPromise = await Post.findByPk(postId);
+        return postPromise;
     }
 
+    // Should this method be static?
     public getPostByUser(user: User, sortBy = 0): Promise<Post> {
         // TODO: Implement logic
         return Promise.reject();
     }
 
-    public getAllPosts(sortBy: number = 0): Promise<Post> {
+    // Should this method be static?
+    public async getAllPosts(sortBy: number = 0): Promise<Post[]> {
+        const allPosts = await Post.findAll();
+        return allPosts;
+    }
+
+    public upvote(post: Post): Promise<Post> {
         // TODO: Implement logic
         return Promise.reject();
     }
 
-    public upvote(): Promise<Post> {
-        // TODO: Implement logic
-        return Promise.reject();
-    }
-
-    public downvote(): Promise<Post> {
+    public downvote(post: Post): Promise<Post> {
         // TODO: Implement logic
         return Promise.reject();
     }
@@ -37,6 +41,12 @@ export class PostService {
         // TODO: Change value of inputparameter of string to int of category enum
         // TODO: Implement logic
         return Promise.reject();
+    }
+
+    // This could also return void, maybe this would make more sense?
+    public async createPost(post: Post): Promise<Post> {
+        const newPost = await Post.create(post);
+        return newPost;
     }
 
     private modifyPost(modifiedPost: Post): Promise<Post> {
