aa845def2d9df1d29918c0df524367d7d53036c6
Stern5497 <62178719+Stern5497@users.noreply.github.com>
2021-10-18 16:19:57 +0200

    create a simple list of posts that is shown.

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/feed/feed.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/feed/feed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../models/user.model";
+import {UserService} from "../services/user.service";
+import {Post} from "../models/Post.module";
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  postList: Post[] = [];
+
+  loggedIn: boolean | undefined;
+
+  user: User | undefined;
+
+  constructor(
+    public userService: UserService
+  ) {
+    // Listen for changes
+    userService.loggedIn$.subscribe(res => this.loggedIn = res);
+    userService.user$.subscribe(res => this.user = res);
+
+    //current Value
+    this.loggedIn = userService.getLoggedIn();
+    this.user = userService.getUser();
+
+    this.postList = this.createPostList();
+  }
+
+  //TODO get the right post list from backend
+
+  //TODO use right model post
+
+
 
   ngOnInit(): void {
+  }
+
+  createPostList(): Post [] {
+    let list: Post[] = [];
+    for(let i = 0; i++, i<5;){
+      list.push(new Post("post" + i));
+    }
+    return list;
   }
 
 }
