e1f04c003ebee943cdf471a15fe26a9af916693e
Martin Arnold <martinleoarnold@gmail.com>
2021-10-21 13:13:20 +0200

    Hook up reactive form with HTML form

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/create-post/create-post.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/create-post/create-post.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { User } from '../models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { UserService } from '../services/user.service';
+import {FormControl, FormGroup, FormBuilder, Validators, ValidationErrors, ValidatorFn, AbstractControl, FormGroupDirective} from '@angular/forms';
+
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css']
 })
-export class CreatePostComponent implements OnInit {
+export class CreatePostComponent{
 
-  constructor() { }
+  createPostForm = this.fb.group({
+    postTitle: new FormControl('', Validators.required),
+    postImage : new FormControl(''),
+    postText : new FormControl('')
+  });
 
-  ngOnInit(): void {
-  }
+  constructor(public httpClient: HttpClient, private fb: FormBuilder) { }
+
+  onSubmit(formDirective: FormGroupDirective): void{}
+
+
+
+
 
 }
