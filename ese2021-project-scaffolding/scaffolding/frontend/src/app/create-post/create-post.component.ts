20c7a5c0fc56d15ce11407d3c2dec065c288380d
Martin Arnold <martinleoarnold@gmail.com>
2021-10-26 16:25:33 +0200

    Implemented http request post/create

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/create-post/create-post.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/create-post/create-post.component.ts
@@ -20,12 +20,61 @@ export class CreatePostComponent{
     postText : new FormControl('')
   });
 
-  constructor(public httpClient: HttpClient, private fb: FormBuilder) { }
 
-  onSubmit(formDirective: FormGroupDirective): void{}
+  file: File;
+
+  filename = '';
+
+  constructor(public httpClient: HttpClient, private fb: FormBuilder, public userService: UserService) { }
+
+  onSubmit(formDirective: FormGroupDirective): void{
+    console.log(this.createPostForm)
+    if(this.createPostForm.valid){
+      this.httpClient.post(environment.endpointURL + "post/create", {
+        title: this.createPostForm.value.postTitle,
+        text: this.createPostForm.value.postText,
+        image: this.createPostForm.value.postImage,
+      }, ).subscribe((res: any) => {
+          console.log(res);
+        },
+        (error: any) =>{
+          console.log(error);
+        });
+    }
+  }
 
 
+  onFileSelected(event: any){
+    this.file = event.target.files[0];
+    this.filename = this.file.name;
+    if(this.createPostForm.value.postImage){
+      console.log(this.filename);
+      console.log(this.createPostForm.value.postImage);
+    }
+  }
 
 
+/*
+  //currently not set
+  checkPost(control: AbstractControl): {[s: string]: boolean}{
+    if(this.createPostForm.value.postImage == "" && this.createPostForm.value.postImage == ""){
+      return {'missingPostContent': true};
+    }
+    return null;
+  };
+  */
 
+
+/*
+  postCompleteValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if(this.createPostForm.value.postImage == "" && this.createPostForm.value.postImage == ""){
+        console.log("error");
+        return {'missingPostContent': true};
+      }
+      console.log("correct");
+      return null;
+    };
+  }
+*/
 }
