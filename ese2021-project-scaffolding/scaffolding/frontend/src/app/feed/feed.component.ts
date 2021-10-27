12a30f995c087d19ffacb03875ce1b3d4bd8c560
Stern5497 <62178719+Stern5497@users.noreply.github.com>
2021-10-27 15:28:09 +0200

    create calls to backend to get list of post, delete a post, get an ordered list not tested!

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/feed/feed.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/feed/feed.component.ts
@@ -4,6 +4,7 @@ import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Post} from "../models/post.model";
 import {Feed} from "../models/feed.model";
+import {User} from "../models/user.model";
 
 
 @Component({
@@ -17,24 +18,32 @@ export class FeedComponent implements OnInit {
 
   currentFeed: Feed = new Feed(0,'', []);
 
+  currentUser: User | undefined;
 
-
+  loggedIn: Boolean | undefined;
 
   constructor(
     public userService: UserService,
     public httpClient: HttpClient
   ) {
+    // Listen for changes
+    userService.loggedIn$.subscribe(res => this.loggedIn = res);
+    userService.user$.subscribe(res => this.currentUser = res);
 
     //current Value
-    this.postList = this.createPostList();
+    this.loggedIn = userService.getLoggedIn();
+    this.currentUser = userService.getUser();
+    // TODO this.readPosts();
   }
 
-  //TODO get the right post list from backend
+  buttonClicked(): void {
+    this.readPosts();
+  }
+
 
   // READ all created posts
   readPosts(): void {
-    //TODO where do I see the right url names?
-    this.httpClient.get(environment.endpointURL + "posts").subscribe((res: any) => {
+    this.httpClient.get(environment.endpointURL + "post/all").subscribe((res: any) => {
       this.currentFeed = new Feed(0,'', []);
       res.forEach((post: any) => {
         //TODO does it make sense to create all new? Is there a possibility to say feed = res
@@ -48,26 +57,43 @@ export class FeedComponent implements OnInit {
   }
 
   // ORDER - Feed
-  //TODO what is the right params?
-  updateList(order: String): void {
-    this.httpClient.put(environment.endpointURL + "posts/" , {
-      order: order
-    }).subscribe();
+  /*
+  1 =
+  2 =
+   */
+  updateList(sortBy: Number): void {
+    this.httpClient.put(environment.endpointURL + "post/all" , {
+      sortBy: sortBy
+    }).subscribe(
+      (res: any) => {
+        this.currentFeed = new Feed(0,'', []);
+        res.forEach((post: any) => {
+            //TODO does it make sense to create all new? Is there a possibility to say feed = res
+            this.currentFeed.posts.push(
+              new Post(0,0,'Post Title','Some text','https://betanews.com/wp-content/uploads/2016/10/game-of-thrones-logo.jpg',0,0,0,'','',0))
+          },
+          (error: any) => {
+            console.log(error);
+          });
+      }
+    );
   }
-
-  //TODO use right model post and feed
-
 
 
   ngOnInit(): void {
   }
 
-  createPostList(): Post [] {
-    let list: Post[] = [];
-    for(let i = 0; i++, i<5;){
-      list.push(new Post(0,0,'Post Title','Some text','https://betanews.com/wp-content/uploads/2016/10/game-of-thrones-logo.jpg',0,0,0,'','',0));
-    }
-    return list;
+
+  deletePost(post: Post): void {
+    this.httpClient.post(environment.endpointURL + "post/delete", {
+      postId: post.postId
+    }).subscribe(() => {
+      this.currentFeed.posts.splice(this.currentFeed.posts.indexOf(post), 1);
+    });
+  }
+
+  updatePost(post: Post): void {
+
   }
 
 }
