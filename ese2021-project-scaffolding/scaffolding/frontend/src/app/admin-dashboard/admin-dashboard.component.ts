7688fbbca4a9e607934b165c47b5de3fa58f7c1d
Martin Arnold <martinleoarnold@gmail.com>
2021-11-24 09:43:57 +0100

    Check access permissions, re-route if no admin

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/admin-dashboard/admin-dashboard.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../models/user.model';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  loggedIn = false;
+  user: User | undefined;
+
+  constructor(
+    public userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+              ) { }
 
   ngOnInit(): void {
+    // Listen for changes
+    this.userService.loggedIn$.subscribe(res => this.loggedIn = res);
+    this.userService.user$.subscribe(res => this.user = res);
+
+    // Current value
+    this.loggedIn = this.userService.getLoggedIn();
+    this.user = this.userService.getUser();
+
+    //reroute if user is not admin
+    this.checkPermissionToAccess();
+  }
+
+  /**
+   * Checks if user is admin, otherwise re-routes.
+   */
+  checkPermissionToAccess(): void {
+    if(!this.user.isAdmin){
+      this.router.navigate(['/feed']).then(r => {});
+    }
   }
 
 }
