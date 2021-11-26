6d5cc52307d7c5e10725f7bbb2c7cbfb37184111
Olivier Mingard <olivier.mingard@students.unibe.ch>
2021-11-26 11:14:07 +0100

    fix sequelize.sync in server.ts, add column names to test data SQL scripts

--- ese2021-project-scaffolding/scaffolding/backend/sql_testdata/product.sql
+++ ese2021-project-scaffolding/scaffolding/backend/sql_testdata/product.sql
@@ -1,2 +1,6 @@
-INSERT INTO product
-VALUES	(1, 'Brightroar', 'https://i.redd.it/8h70o6jtzwd31.jpg', 'Beautiful valyrian steel sword, lost by the Lannisters, exclusive', 400, 1, DATE(), DATE());
+INSERT INTO product(productId, title, image, description, price, productCategory, createdAt, updatedAt)
+VALUES (/* productId: */ 1, /* title: */ 'Brightroar', /* image: */
+                         'https://i.redd.it/8h70o6jtzwd31.jpg', /* description: */
+                         'Beautiful valyrian steel sword, lost by the Lannisters, exclusive', /* price: */
+                         400, /* productCategory: */ 1, /* createdAt: */ DATE (), /* updatedAt: */ DATE ());
+
