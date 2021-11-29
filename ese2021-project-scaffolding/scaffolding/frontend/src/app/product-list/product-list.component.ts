9502f578790e7404117d505d7b9443ce43e2dc3e
MartinaBruetsch <martina.bruetsch@students.unibe.ch>
2021-11-30 00:26:05 +0100

    Changed to a dynamic filtering of the feed

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/product-list/product-list.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/product-list/product-list.component.ts
@@ -9,6 +9,7 @@ import {User} from "../models/user.model";
 import {MatDialog} from "@angular/material/dialog";
 import {ConfirmationDialogModel} from "../ui/confirmation-dialog/confirmation-dialog";
 import {ConfirmationDialogComponent} from "../ui/confirmation-dialog/confirmation-dialog.component";
+import {Post} from "../models/post.model";
 
 @Component({
   selector: 'app-product-list',
@@ -23,6 +24,10 @@ export class ProductListComponent implements OnInit {
   currentUser: User | undefined;
 
   showAddProductButton: boolean = false;
+
+  filterBy: string = '';
+
+  productCategories: string[] = ['Memorabilia', 'Fanart'];
 
   constructor(
     public httpClient: HttpClient,
@@ -69,17 +74,52 @@ export class ProductListComponent implements OnInit {
   // READ all created products
   readProducts(): void {
     this.httpClient.get(environment.endpointURL + "product/all").subscribe((res: any) => {
-      this.currentShop = new ProductList(0,'', []);
+      this.currentShop = new ProductList(0, '', []);
       res.forEach((product: any) => {
-      if (!product.sold){
-        this.currentShop.products.push(
-              new Product(product.productId,0,product.title,product.description,product.image,product.price,product.productCategory,product.sold))
-              }
-          },
-          (error: any) => {
-            console.log(error);
-          });
+        if (this.checkIfProductIsAcceptedByFilter(this.getRightCategory(product.productCategory))) {
+          //const i = this.getRightCategory(product.productCategory);
+          this.currentShop.products.push(
+            new Product(product.productId, 0, product.title, product.description, product.image, product.price, product.productCategory, !product.isAvailable))
+
+        }
       });
+    });
+  }
+
+
+  // TODO --> Abfrage direkt in obigem Ding machen und dann verschachtelt
+  getRightCategory(categoryNumber: number): string {
+    /*let categoryName = '';
+    this.httpClient.get(environment.endpointURL + "category/getById", {
+      params: {
+        categoryId: categoryNumber
+      }
+    }).subscribe((res:any) => {
+        categoryName = res.name;
+        console.log(res);
+        console.log(categoryName);
+      },
+      (error: any) => {
+      console.log(error);
+      }
+    );
+    console.log(categoryName);
+    return categoryName;*/
+
+    switch (categoryNumber){
+      case 1: {
+        return 'Memorabilia';
+      }
+      case 4: {
+        return 'Fanart';
+      }
+      case 3: {
+        return 'Winterfell';
+      }
+      default: {
+        return '';
+      }
+    }
   }
 
   refreshShop() {
@@ -143,5 +183,19 @@ export class ProductListComponent implements OnInit {
     });
   }
 
+  filterShop(event:any) {
+    this.readProducts();
+  }
 
+  checkIfProductIsAcceptedByFilter(category: string): boolean {
+    if (this.filterBy == ''){
+      return true;
+    }
+    else{
+      if (this.filterBy == category){
+        return true;
+      }
+      else return false;
+    }
+  }
 }
