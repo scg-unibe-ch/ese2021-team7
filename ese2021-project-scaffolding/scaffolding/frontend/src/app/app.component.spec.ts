0fa6620f17982dcc55f0c0d54b669800a0adb31b
Stern5497 <62178719+Stern5497@users.noreply.github.com>
2021-12-13 17:19:40 +0100

    app.component.spec.ts test

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/app.component.spec.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/app.component.spec.ts
@@ -61,25 +61,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  /**
+   * Component should be created successfully
+   */
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  /**
+   * Title of App should be Game of Thrones
+   */
+  it(`should have as title 'Game of Thrones'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Game of Thrones');
   });
 
-  /*
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
-  });
-
-   */
 });
