b7b25eee26e861449183a769f95707a732d1de3b
Danilo Bigovic <d.big.be@gmail.com>
2021-12-13 23:29:09 +0100

    Add new order, new product and new short post to testdata

--- ese2021-project-scaffolding/scaffolding/backend/sql_testdata/order.sql
+++ ese2021-project-scaffolding/scaffolding/backend/sql_testdata/order.sql
@@ -1,3 +1,4 @@
 INSERT INTO "order"(orderId, firstName, lastName, street, houseNr, zip, city, paymentOption, orderStatus, user, createdAt, updatedAt, ProductProductId)
-VALUES  (/* orderId: */ 1, 'Joachim', 'Bauer', 'Im Feld', '36', '1234', 'Bauernstadt', /* paymentOption: */ 1, /* orderStatus: */ 1, 5, DATE(), DATE(), 1) /* user: */
-   ,    (2, 'Maximilian', 'von Gutenberg', 'HSG', '1', '9000', 'St. Gallen', 2, 1, 2, /* createdAt: */ DATE (), /* updatedAt: */ DATE (), /* ProductProductId: */ 3);
+VALUES  (/* orderId: */ 1, 'Joachim', 'Bauer', 'Im Feld', '36', '1234', 'Bauernstadt', /* paymentOption: */ 1, /* orderStatus: */ 0, 5, DATE(), DATE(), 1) /* user: */
+   ,    (2, 'Maximilian', 'von Gutenberg', 'HSG', '1', '9000', 'St. Gallen', 2, 1, 2, /* createdAt: */ DATE (), /* updatedAt: */ DATE (), /* ProductProductId: */ 3)
+   ,    (3, 'Khal', 'Drogo', 'Vaes Dothrak', '1', '4568', 'Essos', 2, 2, 4, /* createdAt: */ DATE (), /* updatedAt: */ DATE (), /* ProductProductId: */ 7);
